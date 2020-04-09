4d55d64f3c8b43fb91ba0fc3b971c976fabbef47
Taylor Farley <twfarley88@gmail.com>
2020-04-09 10:45:06 -0400

    initializing project

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var budgetController = (function() {
+    
+   var x=23
+    var add = function(a){
+        return x+a;
+        }
+    
+    var tmp = function()
+    {
+        console.log()
+    }
+    return {
+        publicTest: function(b){
+        console.log(add(b)); },
+        
+        test: function(){
+            
+            console.log(x)
+        }
+    }
+})();
+
+
+var UIController = (function() {
+    
+    //some code
+    
+})();
+
+
+
+var controller = (function(bdgtCTRL,UICtrl) {
+    
+    document.querySelector('.add__btn').addEventListener('click',function(){
+//do something
+    });
+    
+    
+    document.addEventListener('keypress',function(e){
+        console.log(e.key)
+    })
+    
+    
+})(budgetController,UIController);
+
+
