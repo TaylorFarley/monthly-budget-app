1009c40f0509f4d2133be5e56d3684c5f8b8feb3
Taylor Farley <twfarley88@gmail.com>
2020-04-12 12:59:19 -0400

    added dom object and passing it to controller. accepting values for each input

--- app.js
+++ app.js
@@ -6,21 +6,47 @@ var budgetController = (function() {
 })();
 
 
-var UIController = (function() {
- 
-    //some code
+var UIController = (function()
+{
+ var DOMStrings = {
+     inputType: '.add__type',
+     inputDescription: '.add__description',
+     inputValue: '.add__value',
+     inputBtn: '.add__btn'
+     
+ }
     
+    
+return {
+  getInput: function() 
+     {
+    return {
+     type: document.querySelector(DOMStrings.inputType).value,    
+     description: document.querySelector(DOMStrings.inputDescription).value, 
+     value: document.querySelector(DOMStrings.inputValue).value
+           }; 
+      },
+    
+    
+    getDomStrings: function()
+        {
+        return DOMStrings;
+        }
+       }
 })();
 
 
 //GLOBAL APP CONTROLLER
 var controller = (function(bdgtCTRL,UICtrl) {
   
+    var DOM = UICtrl.getDomStrings();
+    
     var ctrlAddItem = function()
     {
         console.log('it works')
          //1. get the field input data
-        
+        var input = UIController.getInput();
+        console.log(input)
         //2. add item to budget controller
         
         //3. add new titem to UI
@@ -29,7 +55,7 @@ var controller = (function(bdgtCTRL,UICtrl) {
         // on git read-input
         //5. display budget on the UI
     }
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem)
     
     
     
